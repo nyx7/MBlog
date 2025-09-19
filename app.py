5f3e50281ac18432f026b94f4af2e50c77982d51
mohamedabayad <mohanedoppo@gmail.com>
2025-09-19 15:31:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -251,4 +251,4 @@ def Update():
         return redirect(url_for('loginform'))
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
